35741a8cc492282493509c7c79f4ec10c517ca05
Lesley Witkamp <lesleywitkamp2@gmail.com>
2021-06-09 11:46:26 +0200

    Fixed version checking code

--- .vu/intermediate/ext/server/__init__.lua
+++ .vu/intermediate/ext/server/__init__.lua
@@ -1,7 +1,7 @@
 --[[
     Venice Unleashed - Intermediate Lua binding
     Type: ext\server\__init__.lua
-    Generated on: Wed Jun 09 2021 10:47:26 GMT+0200 (Central European Summer Time)
+    Generated on: Wed Jun 09 2021 11:46:08 GMT+0200 (Central European Summer Time)
 --]]
 
 ---@meta
--- ext/server/__init__.lua
+++ ext/server/__init__.lua
@@ -1,31 +1,33 @@
-function Server:GetCurrentVersion()
+function GetCurrentVersion()
     -- Version Check Code Credit: https://gitlab.com/n4gi0s/vu-mapvote by N4gi0s
-        options = HttpOptions({}, 10)
-        options.verifyCertificate = false
-        res = Net:GetHTTP("https://raw.githubusercontent.com/lywit/VU-More-Gore/main/mod.json", options)
+    options = HttpOptions({}, 10)
+    options.verifyCertificate = false
+    res = Net:GetHTTP("https://raw.githubusercontent.com/lywit/VU-More-Gore/main/mod.json", options)
 
-        if res.status ~= 200 then
-            return null
-        end
+    if res.status ~= 200 then
+        return null
+    end
 
-        json = json.decode(res.body)
-        return json.Version
+    json = json.decode(res.body)
+    return json.Version
+
+end
+
+function CheckVersion()
+
+    if Server:GetCurrentVersion() ~= localModVersion then
+
+        print("Version: " .. localModVersion)
+        print("This mod seems to be out of date! Please visit https://github.com/lywit/VU-More-Gore");
+        print('Latest version: ' .. json.Version)
+
+    else
+
+        print("Version: " .. localModVersion)
+        print("You're running the lastest version!")
 
     end
 
-    function Server:CheckVersion()
+end
 
-        if Server:GetCurrentVersion() ~= localModVersion then
-
-            print("Version: " .. localModVersion)
-            print("This mod seems to be out of date! Please visit https://github.com/lywit/VU-More-Gore");
-            print('Latest version: ' .. json.Version)
-
-        else
-
-            print("Version: " .. localModVersion)
-            print("You're running the lastest version!")
-
-        end
-
-    end
+CheckVersion()
