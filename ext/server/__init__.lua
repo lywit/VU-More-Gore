f7c4281446f168e12946813a509ade2b88cadc63
Lesley Witkamp <lesleywitkamp2@gmail.com>
2024-05-22 00:43:10 +0200

    Corpse timer is now server sided
    Corpse timer now server sided instead of shared.

--- ext/server/__init__.lua
+++ ext/server/__init__.lua
@@ -57,6 +57,14 @@ Hooks:Install('BulletEntity:Collision', 1, function(hook, entity, hit, giverInfo
     end
 end)
 
+--https://github.com/EmulatorNexus/Venice-EBX/blob/f06c290fa43c80e07985eda65ba74c59f4c01aa0/FX/Impacts/Soldier/Emitter_S/Em_Impact_Soldier_Body_Blood_Chunks_01_S.txt#L287 
+ResourceManager:RegisterInstanceLoadHandler(Guid('F256E142-C9D8-4BFE-985B-3960B9E9D189'), Guid('705967EE-66D3-4440-88B9-FEEF77F53E77'), function(instance)
+	instance = VeniceSoldierHealthModuleData(instance)
+
+	instance:MakeWritable()
+	instance.timeForCorpse = corpseDecayTime
+end)
+
 Events:Subscribe('Soldier:HealthAction', function(soldier, action)
     if action == HealthStateAction.OnRevive then
         NetEvents:Broadcast('RemoveDismemberment', soldier.bus.networkId)
